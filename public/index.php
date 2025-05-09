dce0d55699ba79b08339aa6ed1a11ba0a35abc54
Audinot Noah <audinotnoah@gmail.com>
2025-05-09 11:47:19 +0200

    modif basepath

--- public/index.php
+++ public/index.php
@@ -8,8 +8,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use Slim\App;
 
-$app = AppFactory::create();
+use gift\appli\utils\Eloquent;
 
+try {
+    Eloquent::init(__DIR__ . '/../src/conf/conf.ini');
+} catch (\Exception $e) {
+    echo ($e->getMessage());
+}
+
+$app = AppFactory::create();
+// $app->addRoutingMiddleware();
+$app->setBasePath('/archi/giftbox.squelette/gift.appli/public');
 
 $routesFilePath = __DIR__ . '/../src/conf/routes.php';
 

2787b84c39d3d46ecadc49b622fbb844dbac9dcb
Audinot Noah <audinotnoah@gmail.com>
2025-05-27 14:43:23 +0200

    debut td 6 ex 1 et 2

---
+++ src/application_core/application/exceptions/ValidationException.php
@@ -0,0 +1,6 @@
+<?php
+declare(strict_types=1);
+
+namespace gift\core\domain\exceptions;
+
+class ValidationException extends \Exception {}
---
+++ src/application_core/application/usecases/GestionBoxServiceInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace gift\core\application\usecases;
+
+use gift\core\domain\exceptions\BoxNotFoundException;
+use gift\core\domain\exceptions\PrestationNotFoundException;
+use gift\core\domain\exceptions\CoffretTypeNotFoundException;
+use gift\core\domain\exceptions\ValidationException; 
+use gift\core\domain\exceptions\OperationNotPermittedException; 
+
+interface GestionBoxServiceInterface
+{
+
+    public function creerBoxVide(string $userId, array $dataDonneesBox): array;
+
+    public function creerBoxDepuisType(string $userId, int $coffretTypeId, array $dataDonneesBox): array;
+
+    public function ajouterPrestationABox(string $boxId, string $prestationId, int $quantite, string $userId): array;
+
+    public function retirerPrestationDeBox(string $boxId, string $prestationId, string $userId): array;
+
+    public function modifierQuantitePrestationDansBox(string $boxId, string $prestationId, int $nouvelleQuantite, string $userId): array;
+
+    public function afficherBoxEnCours(string $boxId, string $userId): array;
+
+    public function validerBox(string $boxId, string $userId): array;
+
+}
